32952e2fce4b382c6c19d72430b560373f20d055
Bernardo Vieira Carneiro <bernardo.vieira@walmart.com>
2017-12-20 11:40:03 -0200

    Refactoring

--- src/main/java/domain/lastestReleaseSearch/SearchLatestReleaseResponse.java
+++
@@ -1,9 +0,0 @@
-package domain.lastestReleaseSearch;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class SearchLatestReleaseResponse {
-
-
-}
